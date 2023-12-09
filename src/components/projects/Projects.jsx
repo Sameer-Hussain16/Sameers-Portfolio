d69a113ea47e3506bb21d680b82cbf81662f1f8d
Sameer Hussain <139476793+Sameer-Hussain16@users.noreply.github.com>
2023-12-09 17:22:18 +0530

    Update Projects.jsx

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,21 +3,22 @@ import React from "react"
 
 
 const projects = [
-  {
-    title: "Beautiful Calci",
-    desc: "React.js - Calculator - Web Design",
-    GitHubLink: "https://github.com/Sameer-Hussain16/BeautifulCalci-React"
+    {
+    title: "TaskTracker",
+    desc: "React.js - Task Management - Productivity",
+    GitHubLink: "https://github.com/Sameer-Hussain16/TaskTracker-React"
   },
-  {
+    {
     title: "CliMate",
     desc: "React.js - Weather API - Dynamic",
     GitHubLink: "https://github.com/Sameer-Hussain16/CliMate-React"
   },
   {
-    title: "TaskTracker",
-    desc: "React.js - Task Management - Productivity",
-    GitHubLink: "https://github.com/Sameer-Hussain16/TaskTracker-React"
+    title: "Calci Lite",
+    desc: "React.js - Calculator - Web Design",
+    GitHubLink: "https://github.com/Sameer-Hussain16/BeautifulCalci-React"
   },
+
   {
     title: "UX/UI Design",
     desc: "Figma - Wireframes and Prototypes - Notion",
